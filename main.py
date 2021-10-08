c1da4c617504843c7ea3b20b274dec21a7c5f4b3
abiralp <22109745+abiral001@users.noreply.github.com>
2021-10-08 08:13:38 +0545

    Added data.json

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import discord
 import os
 from dotenv import load_dotenv
+import json
 
 client = discord.Client()
 load_dotenv()
 
 @client.event
 async def on_ready():
-    print("WE have logged in as (0.user)".format(client))
+    print("WE have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('!build %s'):
-        await message.channel.send('Hello! %s')
+    if message.content.startswith('!build'):
+        await message.channel.send('Hello! {0}'.format(message.author))
+        all_data = get_data(message.content.split(' ')[1].lower())
+        await message.channel.send(all_data)
+
+def get_data(character):
+    file = open('./data.json', 'r+')
+    data = json.load(file)
+    return data[character]['element']
 
 client.run(os.getenv('TOKEN'))
