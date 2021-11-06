4372f1ed66fe10ab6db61beadd31c7020bccdc06
abiralp <22109745+abiral001@users.noreply.github.com>
2021-11-06 17:31:22 +0545

    Changed back end completely

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ import os
 from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
+import re
 import json
-import genimp.gtalent as get
+import genimp.genbase as geb
 import val.agents as vaa
 
-URL_GENSHIN_CHARS = "https://library.keqingmains.com/characters"
-DIFF_CSS = ['kokomi', 'thoma', 'mona']
+URL_GENSHIN_CHARS = "https://paimon.moe/client/build.fc5db9a5.js"
+ALTERED_NAMES = {
+    'kokomi': 'sangonomiya_kokomi',
+    'hutao': 'hu_tao',
+    'sara': 'kujou_sara',
+    'raiden': 'raiden_shogun',
+    'shogun': 'raiden_shogun',
+    'baal': 'raiden_shogun',
+    'childe': 'tartaglia',
+    'ayaka': 'kamisato_ayaka',
+    'kazuha': 'kaedehara_kazuha'
+}
 client = discord.Client()
-load_dotenv()
 
-def process_url(character):
-    json_data = open('./genimp/gdata.json', 'r+')
-    json_data = json.load(json_data)
-    if character == 'hutao':
-        character = 'hu-tao'
-    if not character in json_data.keys():
-        return {'error': "No character with such a name found. Please put the actual name of the character"}
-    elif json_data[character]["released"] == 'false':
-        return {'error': "Character is not released or not computed on. No data to show"}
-    return {
-        'character_name': character, 
-        'element': json_data[character]["element"], 
-        'artifacts': json_data[character]["artifacts"],
-        'substat': json_data[character]["substat"],
-        'weapons': json_data[character]["weapons"],
-        'error': "None"
-    }
+load_dotenv()
 
 @client.event
 async def on_ready():
@@ -39,65 +33,86 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('!help'):
+    if message.content.startswith('!cloudshelp'):
         await message.channel.send('You are viewing all the commands of Clouds Bot')
     
     if message.content.startswith('!gbuild'):
         # await message.channel.send('Hello! {0}'.format(message.author))
         print("Request accepted from {}".format(message.author))
-        all_data = get_data(message.content.split(' ')[1].lower())
-        
-        if all_data['message'] != None:
+        response = requests.get(url = URL_GENSHIN_CHARS)
+        response = response.text
+        response = response.replace('const e=', '')
+        response = response.replace('!0', '0')
+        response = response.replace('!1', '1')
+        response = response.replace(';export{e as b};', '')
+        response = response.replace('\n//# sourceMappingURL=build.fc5db9a5.js.map', '')
+        reg = re.findall(r'(\{|\,)([A-z]+)\:', response)
+        list_all = list(set(reg))
+        repeat_check = list()
+        for _, name in list_all:
+            if name not in repeat_check:
+                response = response.replace('{}:\\n'.format(name), '{} \\n'.format(name))
+                response = re.sub(r'(%s)\:'% name, '"{}":'.format(name), response)
+                repeat_check.append(name)
+        as_json = json.loads(response)
+        all_data = parse_data(message.content.split(' ')[1].lower(), as_json)
+        if all_data['message'] != "null":
             await message.channel.send(all_data["message"])
             return
-        display_data = [
-            "Name: {}, Element: {}".format(all_data[0], all_data[1][0].upper()+all_data[1][1:]),
-            "At Level 90",
-            "============",
-            "Base HP: {}, Base ATK: {}, Base DEF:{}, {}:{}".format(all_data[3], all_data[4], all_data[5], all_data[2], all_data[6]),
-            "Recommended Artifacts: {}".format(all_data[7]),
-            "Recommended Mainstat: {}".format(all_data[9]),
-            "Recommended Weapons: {}".format(all_data[8])
-        ]
-        for one_data in display_data:
-            await message.channel.send(one_data)
+        await message.channel.send("Building for {}:".format(all_data['character'].replace('_', ' ')))
+        for one_role in all_data['content']['roles']:
+            await message.channel.send("For the role of {}:".format(one_role))
+            await message.channel.send("=====================================")
+            await message.channel.send("Weapons: (Order is best to worst)")
+            weapons = all_data["content"]["roles"][one_role]["weapons"]
+            for one_weapon in weapons:
+                await message.channel.send(one_weapon["id"].replace('_', ' '))
+            await message.channel.send("-------------------------------------")
+            await message.channel.send("Artifacts: (Order is best to worst)")
+            artifacts = all_data["content"]["roles"][one_role]["artifacts"]
+            for one_artifact in artifacts:
+                await message.channel.send(one_artifact)
+            await message.channel.send("-------------------------------------")
+            await message.channel.send("Artifact Main Stat:")
+            mainStats = all_data["content"]["roles"][one_role]["mainStats"]
+            await message.channel.send(mainStats)
+            await message.channel.send("-------------------------------------")
+            await message.channel.send("Artifact Sub Stat:")
+            subStats = all_data["content"]["roles"][one_role]["subStats"]
+            await message.channel.send(subStats)
+            await message.channel.send("-------------------------------------")
+            await message.channel.send("Talent Priority:")
+            talent = all_data["content"]["roles"][one_role]["talent"]
+            await message.channel.send(talent)
+            # await message.channel.send("-------------------------------------")
+            # await message.channel.send("Tips:")
+            # tip = all_data["content"]["roles"][one_role]["tip"]
+            # await message.channel.send(tip)
+            # await message.channel.send("-------------------------------------")
+            # await message.channel.send("Note:")
+            # note = all_data["content"]["roles"][one_role]["note"]
+            # await message.channel.send(note)
+            await message.channel.send("=====================================")
     
     if message.content.startswith('!gtalent'):
-        
         await message.channel.send('Genshin Impact characters talent are being worked on. Stay Tuned :)')
 
     # Working on valorant agents
     if message.content.startswith('!vagent'):
         await message.channel.send('Valorant agents are being worked on. Please stay tuned {}. :)'.format(str(message.author).split('#')[0]))
 
-def get_data(character):
-
-    data = process_url(character)
-
-    if data['error'] != "None":
-        return {"message": data['error']}
-
-    artifacts = data['artifacts']
-    weapons = data['weapons']
-    substat = data['substat']
-
-    response = requests.get(url = "{}/{}/{}".format(URL_GENSHIN_CHARS, data['element'], data['character_name'].lower()))
-    soup = BeautifulSoup(response.content, 'html.parser')
-    actual_name = [r.strip() for r in soup.select_one('div > span:nth-child(3) > span')]
-
-    if character.lower() in DIFF_CSS:
-        ascension_stat = [r.strip() for r in soup.select_one('#root > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-13awgt0 > div > div > div > div.css-1dbjc4n.r-1ro0kt6.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1777fci > div > div:nth-child(2) > div.css-1dbjc4n.r-bnwqim > div:nth-child(2) > div:nth-child(4) > div > div > div > div > div.css-1dbjc4n.r-150rngu.r-156hn8l.r-z2wwpe.r-rs99b7.r-18u37iz.r-16y2uox.r-1wbh5a2.r-lltvgl.r-buy8e9.r-1sncvnh > div > div:nth-child(1) > div:nth-child(5) > div')]
-    else:
-        ascension_stat = [r.strip() for r in soup.select_one('div > div:nth-child(1) > div:nth-child(5) > div > div > div > span > span')]
-    
-    base_hp = [r.strip() for r in soup.select_one('div > div:nth-child(8) > div:nth-child(2) > div > div > div > span > span')]
-    base_atk = [r.strip() for r in soup.select_one('div > div:nth-child(8) > div:nth-child(3) > div > div > div > span > span')]
-    base_def = [r.strip() for r in soup.select_one('div > div:nth-child(8) > div:nth-child(4) > div > div > div > span > span')]
-    ascension_stat_num = [r.strip() for r in soup.select_one('div > div:nth-child(8) > div:nth-child(5) > div > div > div > span > span')]
-    
-    returning = [actual_name[0], data['element'], ascension_stat[0], base_hp[0], base_atk[0], base_def[0], ascension_stat_num[0], artifacts, weapons, substat]
-    return_data = {key: value for key, value in enumerate(returning)}
-    return_data["message"] = None
-    return return_data
+def parse_data(character, all_data):
+    if character in ALTERED_NAMES.keys():
+        character = ALTERED_NAMES[character]
+    if character not in all_data.keys():
+        return {
+            "message": "No Character with such a name found.",
+            "content": "null"
+        }
+    return {
+        "message": "null",
+        "content": all_data[character],
+        "character": character 
+    }
 
 client.run(os.getenv('TOKEN'))
